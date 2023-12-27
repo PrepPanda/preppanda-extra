ecb6db521772a47cb55dfdff5f3da4ad6699f890
Rajat Nai <rajatnai49@gmail.com>
2023-12-27 22:10:53 +0530

    Store questions into the json file

--- questions-fetcher/python_scripts/questions.py
+++ questions-fetcher/python_scripts/questions.py
@@ -1,5 +1,8 @@
 import docx
 import re
+import json
+from colorama import Fore, Style
+import uuid
 
 def extract_questions_and_options(file_path):
     doc = docx.Document(file_path)
@@ -18,7 +21,7 @@ def extract_questions_and_options(file_path):
             if current_question:
                 questions_and_options.append(current_question)
 
-            current_question = {'question': question_matches[0], 'options': []}
+            current_question = {'id': str(uuid.uuid4()), 'question': question_matches[0], 'options': []}
         elif option_matches and current_question:
             current_question['options'].extend(option_matches)
 
@@ -27,11 +30,26 @@ def extract_questions_and_options(file_path):
 
     return questions_and_options
 
+
+def save_to_json(data, file):
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
+
 if __name__ == "__main__":
     file_path = "../data/test2.docx"
     questions_and_options = extract_questions_and_options(file_path)
-
-    for i, qa_pair in enumerate(questions_and_options, 1):
-        print(f"Question {i}: {qa_pair['question']}")
-        for j, option in enumerate(qa_pair['options'], 1):
-            print(f"Option {j}: {option}")
+    # print(questions_and_options)
+    json_file = "./output/data.json"
+    try:
+        save_to_json(questions_and_options, json_file)
+        print(Fore.GREEN + "Successfully saved to json file")
+    except:
+        print(Fore.RED + "Error saving to json file")
+        print(Style.RESET_ALL)
+    
+    
+    
+    # for i, qa_pair in enumerate(questions_and_options, 1):
+    #     print(f"Question {i}: {qa_pair['question']}")
+    #     for j, option in enumerate(qa_pair['options'], 1):
+    #         print(f"Option {j}: {option}")
